35f891080ac446cf2b716cbfceb7e69ee17d8afa
ddoongmause <ddoongmause@gmail.com>
2020-11-25 17:43:50 +0900

    첫 커밋

---
+++ src/lesson14/ExceptionJava.java
@@ -0,0 +1,22 @@
+package lesson14;
+
+
+public class ExceptionJava {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int x = 10;
+		int y = 0;
+		try {
+			System.out.println(x/y);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		
+		
+		System.out.println("AAA");
+		System.out.println("BBB");
+		System.out.println("CCC");
+	}
+
+}
---
+++ src/lesson14/ThrowsEx.java
@@ -0,0 +1,23 @@
+package lesson14;
+
+
+public class ThrowsEx {
+
+	public static void main(String[] args) throws Exception{
+		// TODO Auto-generated method stub
+		ThrowsEx ex = new ThrowsEx();
+		ex.doA();
+	}
+	
+	public void doA() throws Exception{
+		doB();
+	}
+	public void doB() throws Exception{
+		doC();
+	}
+	public void doC() throws Exception{
+		//Exception �븳湲� �븞源�
+	}
+
+}
+	
