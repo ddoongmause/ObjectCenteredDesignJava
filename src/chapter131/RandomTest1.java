0317e7ed52861821bc5b698e2774e6561f4d1325
yhs <yhs@onlinepowers.com>
2020-11-30 18:02:30 +0900

    P.555 스캐너 할 차례

---
+++ src/chapter131/RandomTest1.java
@@ -0,0 +1,12 @@
+package chapter131;
+
+import java.util.Random;
+
+public class RandomTest1 {
+
+	public static void main(String[] args) {
+		Random random = new Random();
+		int value = random.nextInt();
+		System.out.println(value);
+	}
+}
