c129f911b31a3532aa76eb6d2ad96875c3bdef8a
DDoongMause <DDoongMause@gmail.com>
2020-12-09 00:00:59 +0900

    P.655밑에 부분 할 차례

---
+++ src/chapter14/StackEx.java
@@ -0,0 +1,19 @@
+package chapter14;
+
+import java.util.Stack;
+
+public class StackEx {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Stack<Integer> s = new Stack<Integer>();
+		for(int i=0; i<100; i++) {
+			s.push(i);
+		}
+		System.out.println(s);
+		for(int i=0; i<100; i++) {
+			System.out.println(s.pop());
+		}
+	}
+
+}
