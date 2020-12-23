d28fc85028c27a478adb90563e7c948ec7195425
ddoongmause <DDoongMause@gmail.com>
2020-12-23 23:07:32 +0900

    P.729완

--- src/chapter15/ExcelReader.java
+++ src/chapter15/ExcelReader.java
@@ -1,7 +1,24 @@
 package chapter15;
 
+import java.io.File;
+
+import jxl.Cell;
+import jxl.Sheet;
+import jxl.Workbook;
+
 public class ExcelReader {
 	public static void main(String[] args) {
+		Workbook wb = Workbook.getWorkbook(new File("data.xls"));
+		Sheet sheet  = wb.getSheet(0);
 		
+		int i =0;
+		while(true) {
+			try {
+				Cell cell =sheet.getCell(0, i);
+				i++;
+				System.out.println(cell.getContents());
+			}catch(Exception e) {
+					break;
+			}
 	}
 }
